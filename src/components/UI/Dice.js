ad59ed9b4ad528ce5eedef3ce8bf287cd73521ba
aviavissar <aviavissar@gmail.com>
2021-10-09 21:43:35 +0300

    Update Dice.js

--- src/components/UI/Dice.js
+++ src/components/UI/Dice.js
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Dice = props => {
-    const diceType = 'dice-'+props.number
-    const classes = "fa-layers fa-fw fa-10x " + (props.isSpinning ? 'fa-spin' : '')
+const Dice = {number,isSpinning} => {
+    const diceType = 'dice-'+number
+    const classes = "fa-layers fa-fw fa-10x " + (isSpinning ? 'fa-spin' : '')
 
     return <div style={{fontSize:"1.5vw"}}><span className={classes}>
         <FontAwesomeIcon icon={['fas', 'square']} color="black" transform="shrink-7" fixedWidth/>
